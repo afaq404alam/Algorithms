0835133df41e75e79a55eda1034cdb64410c449a
Afaq Ansari <afaq404alam@gmail.com>
2016-03-27 21:26:08 +0530

    My first commit

---
+++ PowerSet/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PowerSet
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var set = new[] {'a','b','c' };
+      PrintPowerSet(set, set.Length);
+
+      Console.ReadLine();
+    }
+
+    static void PrintPowerSet(char[] set, int setSize)
+    {
+      double powerSetSize = Math.Pow(2, setSize);
+      for (int counter = 0; counter < powerSetSize; counter++)
+      {
+        for (int i = 0; i < setSize; i++)
+        {
+          if ((counter & (1 << i)) > 0)
+          {
+            Console.Write(set[i]);
+          }
+        }
+        Console.Write("\n");
+      }
+    }
+  }
+}
